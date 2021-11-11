1617374ff459ef4c7c9599d9ab0edaf4b85dc286
aarifsa.diwan@gmail.com <aarifsa.diwan@gmail.com>
2021-11-11 17:40:16 -0500

    Init

---
+++ src/main/java/com/etr/tripcalculator/TripCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.etr.tripcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etr/tripcalculator/TripCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etr.tripcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
